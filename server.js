cc7e24a7bacd9b645bef1bce6ca3aa0b944b0e9f
ulugbek-lab <bekievbek02@gmail.com>
2026-04-20 16:25:22 +0900

    Updated project file

--- server.js
+++ server.js
@@ -5,21 +5,23 @@ const app = express();
 const http = require("http");
 
 //1: Entry Codes
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //2: Session Codes
-//3: Views Code
+//3: Views Codes
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4:Routing codes
-app.get("/", function (req, res) {
-  res.send("<h1 style='font-family: Arial, sans-serif'>Hello world </h1>");
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/product", function (req, res) {
-  res.send("<h1 style='font-family: Arial, sans-serif'>Products Page </h1>");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
