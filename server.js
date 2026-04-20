43721966c1e6a7ee9eb45363aac1df9642e7e4fd
ulugbek-lab <bekievbek02@gmail.com>
2026-04-20 23:06:21 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require("express");
 
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 //1: Entry Codes
 
 app.use(express.static("public"));
@@ -19,6 +29,9 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ test: "success" });
+});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 app.get("/", function (req, res) {
   res.render("harid");
