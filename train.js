9a4241c3f9179e1cc55f8596b4587f3647d2e5e9
ulugbek-lab <bekievbek02@gmail.com>
2026-04-24 23:48:39 +0900

    feat: task A is updated

--- train.js
+++ train.js
@@ -1,15 +1,28 @@
+//MIT TASK A
+
+function countLetter(letter, word) {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      count += 1;
+    }
+  }
+  return count;
+}
+console.log(countLetter("o", "football"));
+
 // const { resolveInclude } = require("ejs");
 
 //CALLBACK
 
-const list = [
-  "bee a good student", //0-20
-  "make me proud", //20-30
-  "keep pushing", //30-40
-  "give back to society", //40-50
-  "rest now", //50-60
-  "sleep now ", //60
-];
+// const list = [
+//   "bee a good student", //0-20
+//   "make me proud", //20-30
+//   "keep pushing", //30-40
+//   "give back to society", //40-50
+//   "rest now", //50-60
+//   "sleep now ", //60
+// ];
 // function giveAdvice(a, callback) {
 //   if (typeof a !== "number") callback("pls write a number", null);
 //   else if (a <= 20) callback(null, list[0]);
@@ -84,24 +97,24 @@ const list = [
 // run();
 
 //setInterval
-function giveAdvice(a, callback) {
-  if (typeof a !== "number") callback("pls write a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 1000);
-  }
-}
-console.log("passed here 0");
-giveAdvice(70, (err, data) => {
-  if (err) console.log("error:", err);
-  else {
-    console.log(data);
-  }
-});
-console.log("passed here 1");
+// function giveAdvice(a, callback) {
+//   if (typeof a !== "number") callback("pls write a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
+// console.log("passed here 0");
+// giveAdvice(70, (err, data) => {
+//   if (err) console.log("error:", err);
+//   else {
+//     console.log(data);
+//   }
+// });
+// console.log("passed here 1");
